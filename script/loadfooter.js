4df068954b1c5b674a4d2a85ea3857f1bbedc8cd
fulopati95 <123802176+fulopati95@users.noreply.github.com>
2024-03-20 16:00:46 +0100

    footer is kicserélve no-iframe-re

---
+++ script/loadfooter.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var footerHtml = `
+    <div class="footfelirat">
+        <div style="margin-left: 10px; font-style: italic;">...and the rest is history. Rewritten.</div>
+        <a style="margin-right: 10px;" href="https://twitter.com/fomahun" target="_blank">@fomahun</a>
+    </div>
+    `;
+  
+    document.getElementById('footer-placeholder').innerHTML = footerHtml;
+  });
+  
