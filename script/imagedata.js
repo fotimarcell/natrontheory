c5f1a55ea8dc6c36324a28d6f7f4b37df6ccf263
fulopati95 <123802176+fulopati95@users.noreply.github.com>
2024-11-04 19:57:53 +0100

    jpg

--- script/imagedata.js
+++ script/imagedata.js
@@ -5,7 +5,7 @@ const imageData = [
     { id: 'v_5_blue', src: 'media/v_5_blue.jpg', description: 'Is this the famous Egyptian Blue?' },
     { id: 'v_6_rekhmire', src: 'media/v_6_rekhmire.jpg', description: 'Tomb of Rekhmire mural' },
     { id: 'v_10_molten', src: 'media/v_10_molten.mp4', description: 'Molten natron boiling in a tin can' },
-    { id: 'v_12_sigiriya', src: 'media/v_12_sigiriya.mp4', description: 'Sigiriya, a huge granite rock "carved"' },
+    { id: 'v_12_sigiriya', src: 'media/v_12_sigiriya.jpg', description: 'Sigiriya, a huge granite rock "carved"' },
     { id: 'aaa', src: 'bbb', description: 'ccc' }
     // Vesszőőőő!
 ];
