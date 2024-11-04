88752aae3cb9efad775f9cfdb2b4bb2c62c06f52
fulopati95 <123802176+fulopati95@users.noreply.github.com>
2024-11-04 17:48:38 +0100

    rekhmire

--- script/imagedata.js
+++ script/imagedata.js
@@ -2,6 +2,8 @@ const imageData = [
     { id: 'v_1_pouring', src: 'media/v_1_pouring.mp4', description: 'Pouring molten natron onto a granite slab' },
     { id: 'v_2_scoop', src: 'media/v_2_scoops.jpg', description: 'Scoop marks on and around the Unfinished Obelisk' },
     { id: 'v_4_impala', src: 'media/v_4_impala.jpg', description: 'Impala black granite chemically etched with molten natron' },
-    { id: 'v_5_blue', src: 'media/v_5_blue.jpg', description: 'Is this the famous Egyptian Blue?' }
+    { id: 'v_5_blue', src: 'media/v_5_blue.jpg', description: 'Is this the famous Egyptian Blue?' },
+    { id: 'v_6_rekhmire', src: 'media/v_6_rekhmire.jpg', description: 'Tomb of Rekhmire murial' },
+    { id: 'aaa', src: 'bbb', description: 'ccc' }
     // Vesszőőőő!
 ];
