8aaf80d3fdd3c6937d351b8fdf84a97ac56d47f2
fulopati95 <123802176+fulopati95@users.noreply.github.com>
2024-11-04 16:37:13 +0100

    ahhh

--- script/imagedata.js
+++ script/imagedata.js
@@ -1,5 +1,5 @@
 const imageData = [
-    { id: 'v_1_pouring', src: '../img/granite/natron1.mp4', description: 'Pouring molten natron onto a granite slab' },
+    { id: 'v_1_pouring', src: 'media/v_1_pouring.mp4', description: 'Pouring molten natron onto a granite slab' },
     { id: 'v_2_scoop', src: 'media/v_2_scoopmarks2.jpg', description: 'Scoop marks on and around the Unfinished Obelisk' },
     { id: '3', src: 'image3.jpg', description: 'Leírás a harmadik képről' }
     // Tetszőleges számú elem hozzáadható
