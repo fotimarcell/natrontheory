15ce13608258a34e009632d680aebd2bc02abf03
fulopati95 <123802176+fulopati95@users.noreply.github.com>
2024-11-04 17:25:13 +0100

    még zúm

--- script/imagedata.js
+++ script/imagedata.js
@@ -3,5 +3,5 @@ const imageData = [
     { id: 'v_2_scoop', src: 'media/v_2_scoops.jpg', description: 'Scoop marks on and around the Unfinished Obelisk' },
     { id: 'v_4_impala', src: 'media/v_4_impala.jpg', description: 'Impala black granite chemically etched with molten natron' },
     { id: 'v_5_blue', src: 'media/v_5_blue.jpg', description: 'Is this the famous Egyptian Blue?' }
-    // Tetszőleges számú elem hozzáadható
+    // Vesszőőőő!
 ];
