5cf4f00c9f02928a534e1f4289c36c2626f34dcd
fulopati95 <123802176+fulopati95@users.noreply.github.com>
2024-11-04 17:18:21 +0100

    v_5_blue

--- script/imagedata.js
+++ script/imagedata.js
@@ -1,6 +1,7 @@
 const imageData = [
     { id: 'v_1_pouring', src: 'media/v_1_pouring.mp4', description: 'Pouring molten natron onto a granite slab' },
     { id: 'v_2_scoop', src: 'media/v_2_scoops.jpg', description: 'Scoop marks on and around the Unfinished Obelisk' },
-    { id: 'v_4_impala', src: 'media/v_4_impala.jpg', description: 'Impala black gramite chemically etched with molten natron' }
+    { id: 'v_4_impala', src: 'media/v_4_impala.jpg', description: 'Impala black granite chemically etched with molten natron' }
+    { id: 'v_5_blue', src: 'media/v_5_blue.jpg', description: 'Is this the famous Egyptian Blue?' }
     // Tetszőleges számú elem hozzáadható
 ];
