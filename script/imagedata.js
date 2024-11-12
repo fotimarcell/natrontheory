f85a31078060103c9fa344b89956ea5e76b64bcc
fulopati95 <123802176+fulopati95@users.noreply.github.com>
2024-11-12 18:11:54 +0100

    obelisk

--- script/imagedata.js
+++ script/imagedata.js
@@ -1,4 +1,5 @@
 const imageData = [
+    { id: 'ii_1_obelisk', src: 'media/ii_1_obelisk.mp4', description: 'The Unfinished Obelisk in Aswan, Egypt' },
     { id: 'v_1_pouring', src: 'media/v_1_pouring.mp4', description: 'Pouring molten natron onto a granite slab' },
     { id: 'v_2_scoop', src: 'media/v_2_scoops.jpg', description: 'Scoop marks on and around the Unfinished Obelisk' },
     { id: 'v_4_impala', src: 'media/v_4_impala.jpg', description: 'Impala black granite chemically etched with molten natron' },
