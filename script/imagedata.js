3b34810e06dede983e042ec9fcd6dfde5bc584dd
fulopati95 <123802176+fulopati95@users.noreply.github.com>
2024-11-13 13:44:40 +0100

    coin excalibur

--- script/imagedata.js
+++ script/imagedata.js
@@ -6,6 +6,7 @@ const imageData = [
     { id: 'ii_7_salinity', src: 'media/ii_7_salinity.jpg', description: 'Soil salinization at work' },
     { id: 'ii_8_koh', src: 'media/ii_8_koh.jpg', description: 'Etching granite with potassium hydroxide, 406 degrees Celsius' },
     { id: 'ii_9_chemical', src: 'media/ii_9_chemical.jpg', description: 'Beautiful chemical garden made from waterglass' },
+    { id: 'ii_10_coin', src: 'media/ii_10_coin.jpg', description: 'The first ever Coin Excalibur in the 21st century' },
     { id: 'v_1_pouring', src: 'media/v_1_pouring.mp4', description: 'Pouring molten natron onto a granite slab' },
     { id: 'v_2_scoop', src: 'media/v_2_scoops.jpg', description: 'Scoop marks on and around the Unfinished Obelisk' },
     { id: 'v_4_impala', src: 'media/v_4_impala.jpg', description: 'Impala black granite chemically etched with molten natron' },
