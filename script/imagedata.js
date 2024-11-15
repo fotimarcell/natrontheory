4aae1e755db65b294f2c92412d7481000267f185
fulopati95 <123802176+fulopati95@users.noreply.github.com>
2024-11-15 18:35:48 +0100

    moai

--- script/imagedata.js
+++ script/imagedata.js
@@ -34,6 +34,7 @@ const imageData = [
     { id: 'vi_16_rosetta', src: 'media/vi_16_rosetta.jpg', description: 'Rosette Stone front and back' },
     { id: 'vii_2_woman', src: 'media/vii_2_woman.jpg', description: 'The Pregnant Woman stone in Baalbek, Lebanon' },
     { id: 'vii_3_obelisk', src: 'media/vii_3_obelisk.jpg', description: 'The color of the Unfinished Obelisk is different' },
+    { id: 'vii_4_moai', src: 'media/vii_4_moai.jpg', description: 'Moai statues are carved?' },
     { id: 'aaa', src: 'bbb', description: 'ccc' }
     // Vesszőőőő!
 ];
