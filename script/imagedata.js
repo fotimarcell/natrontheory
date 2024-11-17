644ee8facf103d649ff60755c4fbfe28857a456a
fulopati95 <123802176+fulopati95@users.noreply.github.com>
2024-11-17 15:08:31 +0100

    stone

--- script/imagedata.js
+++ script/imagedata.js
@@ -36,6 +36,7 @@ const imageData = [
     { id: 'vi_17_obelisk', src: 'media/vi_17_obelisk.jpg', description: 'Obelisk of Hathsepsut, Karnak Temple, Luxor' },
     { id: 'vi_18_stone', src: 'media/vi_18_stone.jpg', description: 'Deep depressions on hard basalt, Ollaytantambo, Peru' },
     { id: 'vi_19_bench', src: 'media/vi_19_bench.jpg', description: 'The famous stone bench in Cusco, Peru' },
+    { id: 'vi_20_stone', src: 'media/vi_20_stone.jpg', description: 'Stone in stone, Cusco, Peru' },
     { id: 'vii_2_woman', src: 'media/vii_2_woman.jpg', description: 'The Pregnant Woman stone in Baalbek, Lebanon' },
     { id: 'vii_3_obelisk', src: 'media/vii_3_obelisk.jpg', description: 'The color of the Unfinished Obelisk is different' },
     { id: 'vii_4_moai', src: 'media/vii_4_moai.jpg', description: 'Moai statues are carved?' },
