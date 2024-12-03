5cd4a39cd2243b26ec4182608f24e3f2abd686a8
fulopati95 <123802176+fulopati95@users.noreply.github.com>
2024-12-03 07:50:51 +0100

    excaliburs

--- script/imagedata.js
+++ script/imagedata.js
@@ -23,6 +23,8 @@ const imageData = [
     { id: 'iv_6_scoop', src: 'media/iv_6_scoop.jpg', description: 'Scoop marks in Ollantaytambo, Peru' },
     { id: 'iv_7_dolmen', src: 'media/iv_7_dolmen.jpg', description: 'Beautifully "carved" dolmen in Gelendzhik, Russia' },
     { id: 'iv_8_spirit', src: 'media/iv_8_spirit.mp4', description: 'Bringing forth water from stone' },
+    { id: 'iv_9_excalibur', src: 'media/iv_9_excalibur.jpg', description: 'The Original Excalibur - not' },
+    { id: 'iv_10_galgano', src: 'media/iv_10_galgano.jpg', description: 'The Italian Excalibur in San Galgano abbey, Montesiepsi, Italy' },
     { id: 'v_1_pouring', src: 'media/v_1_pouring.mp4', description: 'Pouring molten natron onto a granite slab' },
     { id: 'v_2_scoop', src: 'media/v_2_scoops.jpg', description: 'Scoop marks on and around the Unfinished Obelisk' },
     { id: 'v_4_impala', src: 'media/v_4_impala.jpg', description: 'Impala black granite chemically etched with molten natron' },
