493b5202866923440c71bb80b3b708746501fae0
fulopati95 <123802176+fulopati95@users.noreply.github.com>
2024-12-03 09:21:03 +0100

    saw

--- script/imagedata.js
+++ script/imagedata.js
@@ -25,6 +25,7 @@ const imageData = [
     { id: 'iv_8_spirit', src: 'media/iv_8_spirit.mp4', description: 'Bringing forth water from stone' },
     { id: 'iv_9_excalibur', src: 'media/iv_9_excalibur.jpg', description: 'The Original Excalibur - not' },
     { id: 'iv_10_galgano', src: 'media/iv_10_galgano.jpg', description: 'The Italian Excalibur in San Galgano abbey, Montesiepsi, Italy' },
+    { id: 'iv_11_saw', src: 'media/iv_11_saw.jpg', description: 'Artificial stone from saw dust' },
     { id: 'v_1_pouring', src: 'media/v_1_pouring.mp4', description: 'Pouring molten natron onto a granite slab' },
     { id: 'v_2_scoop', src: 'media/v_2_scoops.jpg', description: 'Scoop marks on and around the Unfinished Obelisk' },
     { id: 'v_4_impala', src: 'media/v_4_impala.jpg', description: 'Impala black granite chemically etched with molten natron' },
